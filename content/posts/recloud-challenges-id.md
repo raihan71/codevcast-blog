a9d2f72c5350c128b1e6fe1e4f5351d0a642338b
Raihan Nismara <raihannismara@gmail.com>
2021-02-19 13:36:29 +0700

    Codevcast Blog Installed

---
+++ content/posts/recloud-challenges-id.md
@@ -0,0 +1,18 @@
+---
+title: The Biggest Cloud Challenges in Indonesia
+date: 2021-02-07
+published: true
+tags: ['Re_CloudChallenges', 'Re_CloudChallengesIndonesia', 'StayAtHome', 'Codepolitan']
+series: false
+cover_image: https://i.ytimg.com/vi/l7fwuDlbUKY/mqdefault.jpg
+canonical_url: false
+description: "Re-Cloud Challenges Indonesia is the first competition from Alibaba Cloud in Indonesia with the mission of educating the public to get used to using Cloud Service. The event is also supported by online classes and certifications...."
+---
+
+Re-Cloud Challenges Indonesia is the first competition from Alibaba Cloud in Indonesia with the mission of educating the public to get used to using Cloud Service. The event is also supported by online classes and certifications.
+
+This event can be attended by all Indonesians from professionals to students to compete. Carrying the theme Enabling Happiness and Productivity at Home #StayAtHome, Re-Cloud Challenges seeks to challenge all participants to publish their best website work on Alibaba Cloud Server ECS services that support fun and productivity during the Covid-19 pandemic at home.
+
+You can check more detail on their site [Re-Cloud.id](https://re-cloud.id/ "The Biggest Virtual Cloud Challenges")
+
+
---
+++ src/components/Author.vue
@@ -0,0 +1,83 @@
+<template>
+	<div class="author">
+
+		<g-image alt="Author image" :src="logo" style="width: 53%" />
+
+		<h1 v-if="showTitle" class="author__site-title">
+			{{ $static.metadata.siteName }}
+		</h1>
+
+		<p  class="author__intro">
+			Code Developer Cast: {{ $static.metadata.siteDescription }}
+		</p>
+
+		<p class="author__links">
+			<a href="//codevcast.com/" target="_blank">Listen Podcast</a>
+			<a href="//network.codevcast.com/" target="_blank">Join Network</a>
+		</p>
+
+	</div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName,
+		siteDescription
+  }
+}
+</static-query>
+
+<script>
+import dark from '@/assets/images/dark.png';
+import light from '@/assets/images/light.png';
+
+export default {
+	data(){
+		return {
+			logo: light
+		}
+	},
+	props: ['showTitle'],
+	created() {
+		if (window.__theme == 'dark') {
+			this.logo = dark
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.author {
+	margin: 0 auto;
+	max-width: 500px;
+	text-align: center;
+	padding: calc(var(--space) / 2) 0;
+
+	&__image {
+		border-radius: 100%;
+		width: 90px;
+		height: 90px;
+		margin-bottom: 1em;
+	}
+
+	&__logo {
+		width: 50%!important;
+	}
+
+	&__intro {
+		opacity: .8;
+	}
+
+	&__site-title {
+		font-size: 1.5em;
+	}
+
+	&__links {
+		margin-top: -.5em;
+		a {
+			margin: 0 .5em;
+		}
+	}
+}
+</style>
